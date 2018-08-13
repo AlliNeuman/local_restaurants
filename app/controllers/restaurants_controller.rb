0774494afbc3d4706509119e5b8d01295edbff5e
Alli Tzadok <allisontzadok@gmail.com>
2018-08-13 15:10:23 -0500

    add edit link to restaurant show page and add edit form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,7 @@
 class RestaurantsController < ApplicationController
 
+  before_action :set_restaurant, only: [:edit, :show, :update, :destroy ]
+
   def index
     @restaurants = Restaurant.all
   end
@@ -22,7 +24,7 @@ class RestaurantsController < ApplicationController
       redirect_to @restaurant
     else
       render :new
-    end 
+    end
   end
 
   def update
@@ -34,6 +36,11 @@ class RestaurantsController < ApplicationController
   end
 
   private
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
   def restaurant_params
     params.require(:restaurant).permit(:name, :neighborhood, :street_address, :category, :tips)
   end
